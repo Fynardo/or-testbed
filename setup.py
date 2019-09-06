e64a670b0d4f8ad611f2d18e94e7e770e88e6229
Fynardo <dfynar@gmail.com>
2019-09-06 18:23:19 -0400

    packaing issues that pip -e solved for me LUL

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
 
     
 setuptools.setup(
-    name="or_testbed",
+    name="or-testbed",
     version="0.1.0",
     author="Diego Noceda",
     author_email="dfynar@gmail.com",
     description="Operations Research Framework for building metaheuristic algorithms.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     url="https://github.com/Fynardo/or-testbed",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -20,4 +20,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.5',
 )
