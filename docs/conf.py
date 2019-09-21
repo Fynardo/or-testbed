6e1ce4a76109e1380d65476446781575065bccdf
Fynardo <dfynar@gmail.com>
2019-09-21 20:30:13 -0300

    config file out of date

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Diego Noceda'
 author = 'Diego Noceda'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
